83e05cd80192de70b10fca9a762c585a0d8c2f8d
JoseCarlos369 <cfj1010167@est.univalle.edu>
2023-06-07 02:21:44 -0400

    todo

--- Asilo/Program.cs
+++ Asilo/Program.cs
@@ -1,9 +1,19 @@
+using Asilo.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<AsilosAncianosContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
+        )
+    );
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
